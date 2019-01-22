9442a28e26bb494ef8072abfc6f475a28df3451a
shiming.li <lamshiming@sina.com>
2019-01-22 09:28:54 +0800

    Initial commit

---
+++ app/src/main/java/com/lsm/alllibrary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lsm.alllibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 这是我生成libraty的地方
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
